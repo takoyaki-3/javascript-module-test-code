180756f409fcdd3a67bfc3fa1397ae618fb42796
takoyaki-3 <mail@takoyaki3.com>
2023-06-02 01:00:23 +0900

    first commit

---
+++ my_module/index.js
@@ -0,0 +1,7 @@
+// index.js
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+module.exports = addNumbers;
---
+++ my_module/src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+
+function addNumbers(a, b) {
+  return a + b;
+}
+
+const Butter = {
+  addNumbers
+};
+
+module.exports = Butter;
